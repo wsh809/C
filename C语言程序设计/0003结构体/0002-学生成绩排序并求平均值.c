5962f056a458de68718e86e16eaa5f8080392c34
shihang Wang <wsh809@qq.com>
2016-07-02 21:47:20 +0200

    学生成绩排序并求平均值 修改了一下循环排序的部分

--- C语言程序设计/0003结构体/0002-学生成绩排序并求平均值.c
+++ C语言程序设计/0003结构体/0002-学生成绩排序并求平均值.c
@@ -1,20 +1,31 @@
-
+//结构体的作用 
+//在本例中就可以通过交换指针的指向，方便的改变整个结构数组的排列（同时改变id和分数的排序）
 #include<stdio.h>
-void main( )  
-{    
-    struct {
-         char initial;                     /* 姓*/  
-         int age;                            /* 年龄*/
-         int grade;                        /* 考分*/
-        } boy , girl;                      /*两个结构体变量*/
-   boy.initial = 'R';
-   boy.age = 17;
-   boy.grade = 75;
-   girl.age = boy.age - 1;         
-   girl.grade = 82;
-   girl.initial = 'H';
-   printf("%c is %d years old and got a grade of %d\n",
-           girl.initial,    girl.age,    girl.grade);
-   printf("%c is %d years old and got a grade of %d\n",
-           boy.initial,   boy.age,   boy.grade);
+#define STNUM   5
+struct stuinf
+{   int stid;           /* 学生学号 */
+    int score;          /* 学生成绩 */
+}stu [ STNUM ];          /* stu: 结构数组 */
+struct  stuinf *p[STNUM];
+
+void main ( )
+{
+    struct stuinf  *ptemp, *p[STNUM]; int i,j,k,sum=0;
+    for ( i=0; i<=STNUM-1; i++ )
+    {
+        scanf ("%d%d", &stu[i].stid,&stu[i].score);
+        p[i]=&stu[i];
+        sum += stu[i].score;
+    }
+    
+    for ( i =0; i <= STNUM-2; i++ )
+    {
+        for ( j = i; j <= STNUM-1; j++ )
+            if ( p[i]->score < p[j]->score )
+                {  ptemp = p[i];  p[i]=p[j];  p[j]=ptemp; }
+    }
+    
+    for (i=0; i<=STNUM-1; i++)
+        printf("%d,%d\n", (*p[i]).stid, p[i]->score);
+    printf ("average score = %d\n", sum/STNUM);
 }
