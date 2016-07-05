17f54215b24aec9c27d5e6a816ce7677a80c6dc1
shihang Wang <wsh809@qq.com>
2016-07-05 23:17:41 +0200

    1

--- C语言程序设计/0003结构体/0006-链表解决约瑟夫问题.c
+++ C语言程序设计/0003结构体/0006-链表解决约瑟夫问题.c
@@ -5,10 +5,11 @@ struct node
 {
     int num;
     struct node *next;
-}
-typedef struct node NODE; 
+};
+typedef struct node NODE;
 
-void josef(int m, int n)
+void josef(int m, int n);
+void exchange(int a[]);
 
 void main()
 {
@@ -23,6 +24,7 @@ void main()
 void josef(int m, int n)
 {
     int i, k;
+    int a[m/2];
     NODE *head, *p, *q;
     head = (NODE *)malloc(sizeof(NODE));
     head->num = -1;
@@ -42,16 +44,40 @@ void josef(int m, int n)
         p = p->next;
     }
     p->next = head->next;
-    for(i=0; i<15; i++)
+    for(i=0; i<m/2; i++)
     {
-        for(k=1; k<9; k++)
+        for(k=1; k<n; k++)
         {
             p = p->next;
         }
         q = p->next;
         p->next = q->next;
-        printf("%d ", q->num);
+        a[i] = q->num;
         free(q);
+    }
+    exchange(a);
+}
+
+void exchange(int a[])
+{
+    int i,k,m;
+    m = sizeof(a[0]);
+    for(i=m-2; i>=0; i--)
+    {
+        for(k=0;k<=i;k++)
+        {
+            if(a[k]>a[k+1])      //当前面的数比后面的数大时
+            {
+                int temp;      //定义临时变量temp
+                temp = a[k];    //将前面的数赋值给temp
+                a[k] = a[k+1];//前后之数颠倒位置
+                a[k+1] = temp;    //将较大的数放在后面
+            }
+        }
+    }
+    for(i=0;i<m;i++)
+    {
+        printf("%d ", a[i]);
     }
 }
 
