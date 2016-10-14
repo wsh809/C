0c41e9f37c8922e2c9fa2cae21a53560c9adc91b
shihang Wang <wsh809@qq.com>
2016-10-14 11:42:47 +0200

    合并也没有问题了

--- 数据结构/两个有序链表序列的合并.1.c
+++ 数据结构/两个有序链表序列的合并.1.c
@@ -17,33 +17,31 @@ List Merge( List L1, List L2 );
 
 int main()
 {
-    List head;
-    List p;
-    p = (List)malloc(sizeof(List));
-    p->Next = NULL;
-    head = p;
-    
     int m,n;
-    List L1, L2, L;
+    List L1, L2,L;
     printf("数列1的数字个数为：");
     scanf("%d",&m);
     printf("请按从小到大的顺序输入%d个数字：",m);
-    L1 = Read(head,m);
+    L1 = Read(m);
     
     printf("数列2的数字个数为：");
     scanf("%d",&n);
     printf("请按从小到大的顺序输入%d个数字：",n);
-    L2 = Read(head,n);
+    L2 = Read(n);
     
-    L = Merge(L1, L2);
-    // Print(L);
     Print(L1);
+    printf("\r");
     Print(L2);
+    printf("\r");
+    L = Merge(L1, L2);
+    Print(L);
     return 0;
 }
 
-List Read(List head, int i)
+List Read(int i)
 {
+    List head = (List)malloc(sizeof(List));
+    head->Next = NULL;
     List p;
     List q;
     q = head;
@@ -55,16 +53,16 @@ List Read(List head, int i)
         p->Next = NULL;
         q = p;
     }
-    return head;
+    return head->Next;
 }
 
 void Print(List head)
 {
     List p;
-    p = head->Next;
+    p = head;
     while(p != NULL)
     {
-        printf("%d",p->Data);
+        printf("%d ",p->Data);
         p = p->Next;
     }
 }
@@ -72,38 +70,30 @@ void Print(List head)
 
 List Merge(List L1, List L2)
 {
-    List p = L1;
-    List q1 = L2;
-    List q2 = L2->Next;
-    List r = p;
-    while(r != NULL)
+    List head = (List)malloc(sizeof(List));
+    head->Next = NULL;
+    List p = head;
+    while(L1 != NULL && L2!= NULL)
     {
-        p = r;
-        //若L1节点中的数小于 L2节点中的数，则将L1链接在L2该结点之前
-        if(p->Data < q1->Data)
+        if(L1->Data < L2->Data)
         {
-            r = r->Next;
-            p->Next = q1;
-            L2 = p;
-        }
-        else if(p->Data < q2->Data)
-        {
-            r = r->Next;
-            q1->Next = p;
-            p->Next = q2;
-            q1 = q1->Next;
-            q2 = q2->Next;
-        }
-        else if(q1 != NULL)
-        {
-            q1 = q1->Next;
-            q2 = q2->Next;
+            p->Next = L1;
+            L1 = L1->Next;
         }
         else
         {
-            q1->Next = p;
-            return L2;
+            p->Next = L2;
+            L2 = L2->Next;
         }
+        p = p->Next;
     }
-    return L2;
+    if(L1 != NULL)
+    {
+        p->Next = L1;
+    }
+    if(L2 != NULL)
+    {
+        p->Next = L2;
+    }
+    return head->Next;
 }
